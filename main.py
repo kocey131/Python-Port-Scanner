e3b49a3fcb2c627f41619ee10e218003f6e085ad
kocey <kouceila1232@gmail.com>
2024-07-19 23:33:45 +0200

    Added DNS name resolution and improved overall scan time

--- main.py
+++ main.py
@@ -1,35 +1,59 @@
 import socket
 import threading
 from datetime import datetime
+import dns.resolver
 
-cible = input('Cible IP ou localhost:')
+def is_valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+
+# Get the domain name or IP address from the user
+input_value = input('IP or domaine name: ').strip()
+
+# Resolve the domain name to an IP address if necessary
+if is_valid_ip(input_value) or input_value == "localhost":
+    cible = input_value
+else:
+    try:
+        result = dns.resolver.resolve(input_value, 'A')
+        cible = result[0].to_text()
+        print(f"{input_value} Resolved to {cible}")
+    except (dns.resolver.NXDOMAIN, dns.resolver.Timeout, dns.resolver.NoAnswer) as e:
+        print(f"Error in DNS name resolution : {e}")
+        exit(1)
+
 port_debut = 0
-port_fin = 65535
+port_fin = 1023  # Default to scanning well-known [makes the process faster]
 ports_ouverts = []
+ports_ouverts_lock = threading.Lock()
 
-def ports_scan(cible, port):
+def ports_scan_digit(cible, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(1)
         r = s.connect_ex((cible, port))
-        if r == 0:
-            print(f"Port {port} est ouvert.")
-            ports_ouverts.append(port)
-        else:
-            print(f"Port {port} est fermé.")
+        with ports_ouverts_lock:
+            if r == 0:
+                ports_ouverts.append(port)
+                print(f"Port {port} is open.")
+            else:
+                print(f"Port {port} is closed.")
 
-def scanner_ports(cible, port_debut, port_fin):
+def scanner_ports_digit(cible, port_debut, port_fin):
     debut_scan = datetime.now()
     threads = []
     for port in range(port_debut, port_fin + 1):
-        thread = threading.Thread(target=ports_scan, args=(cible, port))
+        thread = threading.Thread(target=ports_scan_digit, args=(cible, port))
         threads.append(thread)
         thread.daemon = True
         thread.start()
 
-    
     for thread in threads:
         thread.join()
 
     duree_scan = datetime.now() - debut_scan
-    print(f"Scan terminer. Ports ouverts : {ports_ouverts} Durée: {duree_scan}.")
+    print(f"Scan done. Open Ports : {ports_ouverts} Durée: {duree_scan}.")
 
-scanner_ports(cible, port_debut, port_fin)
+scanner_ports_digit(cible, port_debut, port_fin)
